a9d0543b6bdeed820656a7ebe32dd54527f0af8c
Aurora Marques <Auroras321@hotmail.com>
2022-07-11 10:29:11 -0300

    Entities added

---
+++ src/com/lululab/entities/Hatsu.java
@@ -0,0 +1,11 @@
+package com.lululab.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Hatsu extends Entity{
+
+	public Hatsu(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+	}
+
+}
---
+++ src/com/lululab/entities/Nen.java
@@ -0,0 +1,11 @@
+package com.lululab.entities;
+
+import java.awt.image.BufferedImage;
+
+public class Nen extends Entity{
+
+	public Nen(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+	}
+
+}
